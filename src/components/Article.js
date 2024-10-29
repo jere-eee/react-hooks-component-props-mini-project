59ed26cef3a1a6a06c23db94bfefdb5c706d5d0b
Akanle Jeremy <akanlejeremy@gmail.com>
2024-10-29 15:41:16 +0300

    still passing

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,14 +1,10 @@
 function Article({ title, date = "January 1, 1970", preview, minutes}){
-    const tea = '☕️'
-    const bento = '🍱'
-    const tcount = Math.round(minutes / 5)
-    const bcount = Math.round(minutes / 10)
     return (
         <article>
             <h3>{title}</h3>
             <small>{date} · {minutes < 30
-                    ? `${tea.repeat(tcount)} ${minutes} min read`
-                    : `${bento.repeat(bcount)} ${minutes} min read`}</small>
+                    ? `${'☕️'.repeat(Math.round(minutes / 5))} ${minutes} min read`
+                    : `${'🍱'.repeat(Math.round(minutes / 10))} ${minutes} min read`}</small>
             <p>{preview}</p>
         </article>
     )
